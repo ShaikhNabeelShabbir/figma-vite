bcc0a6ab3736b81c13ba2d8380275f1b4d03c9ed
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-08-01 12:32:02 +0530

    added drop down

--- src/App.tsx
+++ src/App.tsx
@@ -107,30 +107,30 @@ function App() {
           </div>
         </div>
         {/* hoverCard */}
-      <div
-        className="hoverCard flex items-center space-x-2 w-80 
+        <div
+          className="hoverCard flex items-center space-x-2 w-80 
           border-gray-300 border border-solid bg-white shadow-sm rounded-md
           gap-3.5 p-3"
-      >
-        <Avatar>
-          <AvatarImage src="https://github.com/vercel.png" />
-          <AvatarFallback>VC</AvatarFallback>
-        </Avatar>
-        <div className="space-y-1">
-          <h4 className="text-sm font-semibold">@nextjs</h4>
-          <p className="text-sm">
-            The React Framework – created and maintained by @vercel.
-          </p>
-          <div className="flex items-center pt-2">
-            <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{" "}
-            <span className="text-xs text-muted-foreground">
-              Joined December 2021
-            </span>
+        >
+          <Avatar>
+            <AvatarImage src="https://github.com/vercel.png" />
+            <AvatarFallback>VC</AvatarFallback>
+          </Avatar>
+          <div className="space-y-1">
+            <h4 className="text-sm font-semibold">@nextjs</h4>
+            <p className="text-sm">
+              The React Framework – created and maintained by @vercel.
+            </p>
+            <div className="flex items-center pt-2">
+              <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{" "}
+              <span className="text-xs text-muted-foreground">
+                Joined December 2021
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      {/* dropdown */}
-              <div className="DropDown">
+        {/* dropdown */}
+        <div className="DropDown">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="outline">Open</Button>
